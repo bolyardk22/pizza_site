13536af7bc2c582feb378fa977ad610190366dcb
bolyardk22 <collinsk22@mycu.concord.edu>
2017-08-22 13:47:39 -0400

    removed extra lines

--- app.rb
+++ app.rb
@@ -2,16 +2,5 @@ require 'sinatra'
 enable :sessions
 
 get '/' do
-  erb :sandwich_parts
-end
-
-post '/build' do
-  parts = params[:parts]
-  erb :sandwich_results, :locals=>{:parts=>parts}
-end
-
-get '/correct' do
-    parts = params[:parts]
-    toppings = params[:toppings]
-    erb :areyousure, :locals=>{:parts=>parts, :toppings=>toppings}
+  erb :index
 end
