84a5ae5f7b1b7c73518914587bd0aa413f735bcf
bolyardk22 <collinsk22@mycu.concord.edu>
2017-08-22 16:01:56 -0400

    found pictures; will add them next

--- app.rb
+++ app.rb
@@ -8,3 +8,21 @@ end
 get '/topping_chooser' do
 	erb :toppins
 end
+
+post '/topping_chooser' do
+	session[:sizes] = params[:size]
+	session[:crusts] = params[:crust]
+	session[:sauces] = params[:sauce]
+	session[:cheeses] = params[:cheese]
+	session[:meats] = params[:meat]
+	session[:veggies] = params[:veggie]
+
+	redirect '/confirm?'
+end
+
+get '/confirm' do
+	erb :confirm, locals:{size: session[:sizes], crust: session[:crusts], sauce: session[:sauces], cheese: session[:cheeses], meat: session[:meats], veggie: session[:veggies]}
+end
+
+post '/confirm' do
+end
