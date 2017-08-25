2e999cc5b92a2b0144059aec3697bb8ff4f7d15f
bolyardk22 <collinsk22@mycu.concord.edu>
2017-08-25 14:02:31 -0400

    almost working

--- app.rb
+++ app.rb
@@ -6,12 +6,6 @@ get '/' do
 end
 
 get '/topping_chooser' do
-# 	session[:sizes] = params[:size]
-# 	session[:crusts] = params[:crust]
-# 	session[:sauces] = params[:sauce]
-# 	session[:cheeses] = params[:cheese]
-# 	session[:meats] = params[:meat]
-# 	session[:veggies] = params[:veggie]
  	erb :toppins
 end
 
@@ -39,15 +33,17 @@ get '/confirm' do
 end
 
 post '/blah' do
-	session[:sizes] = params[:size].to_a
-	session[:crusts] = params[:crust]
-	session[:sauces] = params[:sauce]
-	session[:cheeses] = params[:cheese]
-	session[:meats] = params[:meat]
-	session[:veggies] = params[:veggie]
+
+	session[:size_choice] = params[:size_radio]
+	session[:crust_choice] = params[:crust_radio]
+    session[:sauce_choice] = params[:sauce_radio]
+    session[:cheese_choice] = params[:cheese_radio]
+    session[:meat_choice] = params[:meat_radio]
+    session[:veggie_choice] = params[:veggie_radio]
+
 	redirect '/the_end'
 end
 
 get '/the_end' do
-	erb :final
+	erb :final, locals:{size: session[:size_choice], crust: session[:crust_choice], sauce: session[:sauce_choice], meat: session[:meat_choice], veggie: session[:veggie_choice], cheese: session[:cheese_choice]}
 end
